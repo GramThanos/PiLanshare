8e6ef1f501c6a755b5ab60b0210e9cd690818768
gramthanos <gramthanos@gmail.com>
2023-12-12 10:24:02 +0200

    Updated jQuery to v3.7.1

--- webui/index.php
+++ webui/index.php
@@ -152,7 +152,7 @@
 		</div>
 
 		<!-- jQuery & Popper -->
-		<script src="js/jquery-3.4.1.min.js"></script>
+		<script src="js/jquery.min.js"></script>
 		<script src="js/popper.min.js"></script>
 		<!-- Bootstrap -->
 		<script src="js/bootstrap.min.js"></script>
